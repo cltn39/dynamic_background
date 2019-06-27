41c0c193e55f172b850874cecaa179be2184c476
gravitation <chisong@ucdavis.edu>
2019-06-26 22:29:40 -0700

    added package.json

--- canvas-resize/canvas.js
+++ canvas-resize/canvas.js
@@ -1,9 +1,11 @@
-var canvas = document.querySelector('canvas');
+//setting the canvas size to equal to window size
+const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var c = canvas.getContext('2d');
+const c = canvas.getContext('2d');
 
+// c.fillRect(x, y, width, height);
 // c.fillStyle = 'red';
 // c.fillRect(100, 100, 100, 100);
 // c.fillStyle = 'blue';
